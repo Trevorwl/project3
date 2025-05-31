cba8b5fc283fd476be1db677a22d60e12bff278c
trevor <tlash1995@gmail.com>
2025-05-31 15:27:17 -0700

    added disk_creator.c

--- libfs/utilities.c
+++ libfs/utilities.c
@@ -1,9 +1,26 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
 
+#include "disk.h"
+#include "fs.h"
 #include "utilities.h"
+
+uint8_t* utilities_buffer = NULL;
+
+struct __attribute__((__packed__)) metadata{
+    uint8_t signature[8];
+    uint16_t totalBlocks;
+    uint16_t rootDirectoryIndex;
+    uint16_t dataStartIndex;
+    uint16_t totalDataBlocks;
+    uint8_t totalFatBlocks;
+};
 
 void hex_dump(void *data, size_t length) {
     unsigned char *byte = (unsigned char *)data;
@@ -42,5 +59,70 @@ void hex_dump(void *data, size_t length) {
             printf("|\n");
         }
     }
+}
+
+int create_disk(size_t data_blocks,char* filename){
+    if(data_blocks==0 || data_blocks > 8198){
+        printf("create_disk: invalid data block total, valid data block total [1,8198]\n");
+        return -1;
+    }
+
+    int fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0644);
+
+    if (fd == -1) {
+       printf("create_disk: file already exists\n");
+       return -1;
+    }
+
+    size_t fat_blocks = data_blocks / (size_t)FAT_ENTRIES;
+
+    if(data_blocks % FAT_ENTRIES !=0){
+        fat_blocks += 1;
+    }
+
+    if(utilities_buffer==NULL){
+        utilities_buffer = (uint8_t*)calloc(1, bounce_buffer_size);
+     } else {
+        memset(utilities_buffer,0,bounce_buffer_size);
+     }
+
+    size_t disk_blocks = 1 + fat_blocks + 1 + data_blocks;
+
+    for(size_t block = 0; block < disk_blocks; block++){
+        write(fd, utilities_buffer, bounce_buffer_size);
+        memset(utilities_buffer,0,bounce_buffer_size);
+    }
+
+    struct metadata* metadata=(struct metadata*)utilities_buffer;
+
+    char* diskFormat="ECS150FS";
+
+    memcpy(metadata->signature, diskFormat, strlen(diskFormat));
+
+    metadata->totalBlocks=(uint16_t)disk_blocks;
+    metadata->rootDirectoryIndex= (uint16_t)1 + (uint16_t)fat_blocks;
+    metadata->dataStartIndex = (uint16_t)1 + (uint16_t)fat_blocks +  (uint16_t)1;
+    metadata->totalDataBlocks=(uint16_t)data_blocks;
+    metadata->totalFatBlocks=(uint8_t)fat_blocks;
+
+    lseek(fd, 0 , SEEK_SET);
+
+    write(fd, utilities_buffer, bounce_buffer_size);
+
+    memset(utilities_buffer,0,bounce_buffer_size);
+
+    uint16_t* fat_ptr=(uint16_t*)utilities_buffer;
+
+    *fat_ptr = FAT_EOC;
+
+    lseek(fd, BLOCK_SIZE , SEEK_SET);
+
+    write(fd, utilities_buffer, bounce_buffer_size);
+
+    close(fd);
+
+    printf("created disk with %zu data blocks\n",data_blocks);
+
+    return 0;
 }
 
--- libfs/utilities.h
+++ libfs/utilities.h
@@ -2,7 +2,11 @@
 #ifndef UTILITIES_H_
 #define UTILITIES_H_
 
+#include <sys/types.h>
+
 void hex_dump(void *data, size_t length);
+
+int create_disk(size_t data_blocks,char* filename);
 
 
 #endif
