745647b337f74533d4c9e8ed5812b6b894c1ae81
Adriano Melo Filho <adrianomelo1997@gmail.com>
2025-06-02 11:03:58 -0700

    Implemented FD Table: Initializer, isOpenByName

--- libfs/fdTable.c
+++ libfs/fdTable.c
@@ -5,18 +5,35 @@
 #include <stdbool.h>
 #include <string.h>
 
+struct fdTable* init_fd_table() {
+    struct fdTable* table = malloc(sizeof(struct fdTable));
+    if (!table) return NULL;
+    table->fdsOccupied = 0;
+    for (int i = 0; i < FS_OPEN_MAX_COUNT; i++) {
+        table->fdTable[i] = NULL;
+    }
+    return table;
+}
+
 struct fdNode* getFdEntry(struct fdTable* fdTable,int fd){
     struct fdNode* fdNode=fdTable->fdTable[fd];
-
     return fdNode;
 }
 
 bool isOpenByFd(struct fdTable* fdTable,int fd){
     struct fdNode* fdNode=fdTable->fdTable[fd];
-
     return fdNode->in_use==true;
 }
 
-
+bool isOpenByName(struct fdTable *table, const char *filename)
+{
+    for (int i = 0; i < FS_OPEN_MAX_COUNT; i++) {
+        struct fdNode *node = table->fdTable[i];
+        if (node && node->in_use && strcmp(node->filename, filename) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
 
 
